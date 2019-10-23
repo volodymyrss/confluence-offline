a70b7ccb7307c2cf16b1d9b8a2bcc9bae34c6fc0
Volodymyr <vladimir.savchenko@gmail.com>
2019-10-23 15:04:33 +0200

    commit and push

--- metaconf.py
+++ metaconf.py
@@ -1,7 +1,10 @@
 
+import re
+import json
 import requests
 import click
 import keyring
+import dateutil.parser
 
 @click.group()
 @click.option('--docid', type=int)
@@ -19,10 +22,54 @@ def get_auth():
 
 @cli.command()
 @click.pass_context
-def getbody(ctx):
-    r=requests.get("https://issues.cosmos.esa.int/socciwiki/rest/api/content/{docid}?expand=body".format(docid=ctx.obj['docid']), auth=get_auth())
+def pull(ctx):
+    r=requests.get("https://issues.cosmos.esa.int/socciwiki/rest/api/content/{docid}?expand=body.storage".format(docid=ctx.obj['docid']), auth=get_auth())
 
-    click.echo(r.text)
+    body = r.json()['body']['storage']['value']
+
+    open("main.xhtml", "wt").write(body)
+
+@cli.command()
+@click.option('--commit', type=bool, default=False, is_flag=True)
+@click.pass_context
+def push(ctx, commit):
+    r=requests.get("https://issues.cosmos.esa.int/socciwiki/rest/api/content/{docid}?expand=body.storage,version".format(docid=ctx.obj['docid']), auth=get_auth())
+
+    last_version = r.json()['version']
+    print(last_version['by']['displayName'])
+    print(last_version['when'])
+
+    body = r.json()['body']
+
+    body_storage = open("main.xhtml").read()
+
+    dt = dateutil.parser.parse(last_version['when'])
+
+    updated_body = re.sub("(\|\| updated \|)(.*?)\|","\\1 "+dt.strftime("%Y/%m/%d")+" |", body_storage)
+
+    click.echo(updated_body)
+
+    body['storage']['value'] = updated_body
+
+    headers = {
+       "Accept": "application/json",
+       "Content-Type": "application/json"
+    }
+                    
+    data = dict(
+                version=dict(number=last_version['number']+1),
+                title=r.json()['title'],
+                type="page",
+                body=body,
+            )
+    
+    if commit:
+        r=requests.put("https://issues.cosmos.esa.int/socciwiki/rest/api/content/{docid}".format(docid=ctx.obj['docid']), 
+                        headers=headers,
+                        data=json.dumps(data),
+                        auth=get_auth())
+
+        print(r,r.text)
 
 @cli.command()
 def history():
